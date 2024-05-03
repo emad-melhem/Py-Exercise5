41f898200a18f2f158ce40853475010e4e9d8997
Imad Mohamad <yehya.mh@live.com>
2024-05-03 15:22:08 +0200

    Add exercise 3

---
+++ Exercise3.py
@@ -0,0 +1,36 @@
+import math
+
+list_1 = input("Enter a list 'more than one!!\n"
+                    +"'put a space between them' :").split()
+      # Exercise 3.1
+# creat new list that have numbers and strings.
+new_list=[]
+for x in list_1:
+    if x.isnumeric():
+        new_list.append(int(x))
+    else:
+        new_list.append(x)
+        
+if len(new_list) > 1:
+    new_list_length= math.ceil(len(new_list) / 2)
+    new_list1= new_list[:new_list_length:]
+    new_list2= new_list[new_list_length::]
+    print(f"my_lst = {new_list}")
+    print(f"Tuple 1 is: {tuple(new_list1)}")
+    print(f"Tuple 2 is: {tuple(new_list2)}")
+    
+      # begin for Exercise 3.2
+    def IsOdd(number):
+        if number % 2 != 0:
+            return True
+        return False
+    odd_index_list = [j for i, j in enumerate(new_list) if IsOdd(i)]
+    even_index_list = [j for i, j in enumerate(new_list) if not IsOdd(i)]
+    
+    print(f"Tuple 3 is: {tuple(even_index_list)}")
+    print(f"Tuple 4 is: {tuple(odd_index_list)}")
+    
+    # end for exercise 3.2
+    
+else:
+    print("The length of list must contain at least two element!")
