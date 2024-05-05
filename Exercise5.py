851fc29db1fc4804eb64abd612c8a6c3374f88b8
Imad Mohamad <yehya.mh@live.com>
2024-05-05 21:07:38 +0200

    Modified Ex5

--- Exercise5.py
+++ Exercise5.py
@@ -6,13 +6,13 @@ try:
     for x in list_1:
         sum_numbers+= x
     avg= sum_numbers/len(list_1)
-    print(f"The average is {avg:2}")
+    print(f"The average is {avg:.2f}")
     
     # exercise 5.2
     new_num_added= 5 * (len(list_1) + 1) - sum_numbers
     print(f"The average was {avg}")
     list_1.append(new_num_added)
     print(f"Added {new_num_added}, new list is {list_1}")
-    print(f"The average is now {(sum_numbers+new_num_added) / len(list_1)}")
+    print(f"The average is now {(sum_numbers+new_num_added) / len(list_1):.2f}")
 except Exception as err:
     print(err.args[0])
