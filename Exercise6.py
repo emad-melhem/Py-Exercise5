a5d7a1d0ed18081331843cfd30f9e22ec8d4b5b9
Imad Mohamad <yehya.mh@live.com>
2024-05-05 21:16:22 +0200

    Modified Ex6

--- Exercise6.py
+++ Exercise6.py
@@ -1,3 +1,13 @@
+# exercise with additional methods
 
-list_1 = [[3]* 6] * 6
-print(list_1)
+try:
+    num_1 = int(input("Enter ur number: "))
+    num_2 = int(input("Enter the length of array: "))
+    num_3 = int(input("Enter the length of the matrix array: "))
+
+    # here is the exercise.
+    list_1 = [[num_1]* num_2] * num_3
+    print(list_1)
+
+except ValueError:
+    print("Input error!!\nYou must enter numbers only.")
